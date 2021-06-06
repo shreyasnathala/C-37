c0697eff3593408167770483df14afcdae43da4c
shreyasnathala <70395653+shreyasnathala@users.noreply.github.com>
2021-06-06 13:55:41 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var database;
+var allPlayers;
+var gameState=0;
+var playerCount;
+var form,player,game;
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game=new Game();
+  game.getState();
+  game.start()
+  
+}
+
+function draw(){
+  background("white");
+  if(playerCount===4) {
+    game.update(1)
+  }
+    if (gameState---1) {
+      clear();
+      game.play();
+      
+    }
+  
+}
+
